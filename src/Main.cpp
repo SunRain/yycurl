e06a99d09636e45a4ffc6544e407a9c95f5c2815
war10ck-efz <war10ck.efz@gmail.com>
2014-01-13 18:41:21 +0800

    version 1.0 beta

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,40 +11,40 @@
 
 bool STOP_ALL = false;
 
-void gao(int &num, std::vector<std::string> &url, std::vector<std::string> &path){
-	std::cout << "number of download: ";
-	std::cin >> num;
-	for(int i = 0; i < num; i++){
-		std::string new_url, new_path;
-		std::cout << "url: ";
-		std::cin >> new_url;
-		std::cout << "download to: ";
-		std::cin >> new_path;
-		url.push_back(new_url);
-		path.push_back(new_path);
-	}
-}
-
-//void test(int &num, std::vector<std::string> &url, std::vector<std::string> &path){
-//	num = 6;
-//	for(int i = 0; i < 6; i++){
-//		url.push_back("http://download.gamed9.com/package/ttcjh/1.0/ttcjh.apk");
-//		std::stringstream ostr;
-//		ostr << i;
-//		std::string tmp= "/tmp/" + ostr.str();
-//		path.push_back(tmp);
+//void gao(int &num, std::vector<std::string> &url, std::vector<std::string> &path){
+//	std::cout << "number of download: ";
+//	std::cin >> num;
+//	for(int i = 0; i < num; i++){
+//		std::string new_url, new_path;
+//		std::cout << "url: ";
+//		std::cin >> new_url;
+//		std::cout << "download to: ";
+//		std::cin >> new_path;
+//		url.push_back(new_url);
+//		path.push_back(new_path);
 //	}
 //}
+
+void test(int &num, std::vector<std::string> &url, std::vector<std::string> &path){
+	num = 1000;
+	for(int i = 0; i < 1000; i++){
+		url.push_back("http://b.zol-img.com.cn/desk/bizhi/image/1/1920x1200/1349683516475.jpg");
+		std::stringstream ostr;
+		ostr << i;
+		std::string tmp= "/tmp/" + ostr.str() + ".jpg";
+		path.push_back(tmp);
+	}
+}
 
 int main(){
 	int num;
 	std::vector<std::string> url;
 	std::vector<std::string> path;
-	gao(num, url, path);
-//	test(num, url, path);
+//	gao(num, url, path);
+	test(num, url, path);
 	FetchData fetch(num, url, path);
 	ConsoleDisplay console(&fetch);
-	fetch.set_work_num(3);
+	fetch.set_work_num(5);
 	fetch.start();
 	console.start();
 	fetch.join();
